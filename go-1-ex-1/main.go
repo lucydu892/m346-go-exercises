6f7f5baa9fd84bbeb12052f088ac9a579039cb00
Lucy Duss <lucyduss@outlook.com>
2025-11-03 14:28:51 +0100

    Update main.go
    Aufgabe 1 gelöst

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: Declare and initialize the variables being used in the output!
+	firstName := "Lucy"
+	lastName := "Duss"
+	dayOfBirth := 05
+	monthOfBirth := 01
+	yearOfBirth := 2007
+	numberOfSiblings := 0
+	heightInMeters := 1.75
+	zodiacSign := "Capricorn"
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
