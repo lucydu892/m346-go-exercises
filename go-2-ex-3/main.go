645e76d65a9e6502d5121c1e7c32b5793f0e8aa6
Lucy Duss <lucyduss@outlook.com>
2025-11-17 15:11:00 +0100

    Aufgabe 2-4 gelöst

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -5,12 +5,20 @@ import "fmt"
 func main() {
 	// TODO: create a map called "modules"
 
+	modules := map[int]string {
+		104 : "Applikation entwickeln",
+		117 : "Datenbank",
+		346 : "Cloud-Computing",
+	}
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
 	// TODO: delete one
+	delete(modules, 117)
 	// TODO: add one
+	modules[320] = "OOP";
 	// TODO: replace one
+	modules[104] = "Geschäftsprozesse"
 	fmt.Println(modules)
 }
--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -1,8 +1,62 @@
 package main
 
+import "fmt"
+
+type Student struct {
+	firstname string
+	lastname  string
+}
+type Class struct {
+	Students []Student
+}
+type Module struct {
+	Number  int
+	Classes []Class
+}
+
 func main() {
 	// TODO: declare a type for Student (with first and last name)
+
+	class1 := Class{
+		Students: []Student{
+			{firstname: "Jessica", lastname: "Meier"},
+			{firstname: "Leo", lastname: "Holzer"},
+			{firstname: "Carol", lastname: "Pfister"},
+		},
+	}
+	class2 := Class{
+		Students: []Student{
+			{firstname: "Peter", lastname: "Christen"},
+			{firstname: "Sarah", lastname: "Duss"},
+			{firstname: "Noel", lastname: "Hermann"},
+		},
+	}
 	// TODO: declare a type for Class (consisting of multiple students)
+
 	// TODO: declare a map of modules being attended by multiple classes
+	modules := map[int]Module{
+		346: {
+			Number:  346,
+			Classes: []Class{class1},
+		},
+		104: {
+			Number:  104,
+			Classes: []Class{class1, class2},
+		},
+		117: {
+			Number:  117,
+			Classes: []Class{class2},
+		},
+	}
 	// TODO: output everything using fmt.Println()
+	for moduleNumber, module := range modules {
+		fmt.Printf("Modul %d:\n", moduleNumber)
+		for i, class := range module.Classes {
+			fmt.Printf("  Klasse %d:\n", i+1)
+			for _, student := range class.Students {
+				fmt.Printf("    - %s %s\n", student.firstname, student.lastname)
+			}
+		}
+		fmt.Println()
+	}
 }
